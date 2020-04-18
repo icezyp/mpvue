cc1e1c35b347f33a7429e10476ac0fefa875ffa0
zhouyanping <331153148@qq.com>
2020-04-18 17:55:34 +0800

    global component

---
+++ src/components/global/Global.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="global">
+        <div class="error" v-if="error">
+            <div class="error-msg-wrapper">
+                <div class="error-image">
+
+                </div>
+            </div>
+        </div>
+        <slot v-else></slot>
+    </div>
+</template>
+<script>
+import store from '@/store/index'
+
+export default {
+    computed: {
+        error() {
+            return store.state.error
+        }
+    }
+}
+</script>
